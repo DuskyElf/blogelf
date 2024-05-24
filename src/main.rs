aeadbcfa26c37331de1622987ee81a10a485298e
Pranjal-Patel <pranjalpatel9838@gmail.com>
2024-05-24 11:30:02 +0530

    update cli

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,68 @@
-use std::collections::HashMap;
-use std::env;
-use std::process::exit;
+use clap::Parser;
 
 mod commands;
 
-use commands::*;
+pub type BlogELFError = Box<dyn std::error::Error>;
+pub type BlogELFResult = Result<(), BlogELFError>;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Debug, Parser)]
+enum SubCommand {
+    /// Create a new project in an existing directory
+    Init,
+
+    /// Create a new project in a new directory
+    New { name: String },
+
+    /// Render the project into a static site
+    Render {
+        /// The markdown file
+        input: String,
+
+        /// The output html file
+        output: String,
+
+        /// Template html file
+        template: String,
+    },
+
+    /// Create a new blog post template
+    Template {
+        // TODO: add doc comments
+        input: String,
+        output: String,
+
+        /// <key>=<value> pairs to replace in the template
+        args: Vec<String>,
+    },
+}
 
 fn main() {
-    let result = start();
+    let args = Args::parse();
 
-    if let Err(err) = result {
-        eprintln!("Error: {err}");
-        exit(-1);
+    if let Err(e) = match args.subcmd {
+        SubCommand::Init => commands::init(&std::env::current_dir()
+            .expect("could not get current directory")),
+
+        SubCommand::New { name } => commands::new(name),
+        SubCommand::Render {
+            input,
+            output,
+            template,
+        } => commands::render(&input, &output, &template),
+
+        SubCommand::Template {
+            input,
+            output,
+            args,
+        } => commands::template(&input, &output, args),
+    } {
+        eprintln!("[Error]: {e}");
     }
-}
-
-fn start() -> Result<(), String> {
-    let mut commands: HashMap<&str, &dyn Command> = HashMap::new();
-    commands.insert("init", &init::Init);
-    commands.insert("new", &new::New);
-    commands.insert("render", &render::Render);
-    commands.insert("template", &template::Template);
-
-    let (command, args) = parse_args_command(&commands)?;
-    command.run(args)?;
-
-    Ok(())
-}
-
-fn parse_args_command<'cmd>(
-    commands: &HashMap<&str, &'cmd dyn Command>,
-) -> Result<(&'cmd dyn Command, env::Args), &'static str> {
-    let mut args = env::args();
-    let _prog_name = args.next().expect("where's program name");
-
-    let Some(command) = args.next() else {
-        return Err("No Command Provided");
-    };
-
-    let Some(command_obj) = commands.get(&*command) else {
-        return Err("Invalid Command");
-    };
-
-    Ok((*command_obj, args))
 }
