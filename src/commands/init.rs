65f9b71f54e9b9db8d46dffc48c5247820575202
DuskyElf <91879372+DuskyElf@users.noreply.github.com>
2024-05-24 15:04:41 +0530

    Correct setup of Config file

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -1,19 +1,38 @@
 use include_dir::{include_dir, Dir};
-use std::path::Path;
+use std::{collections::HashMap, fs, path::Path};
 
 use crate::{BlogELFError, BlogELFResult};
 
+use super::*;
+
+const CONFIG_NAME: &str = "BlogElf.toml";
 static STATIC_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR/static");
 
 pub fn init(path: &Path) -> BlogELFResult {
-    let _ = path
+    STATIC_DIR
+        .extract(&path)
+        .map_err(|err| -> BlogELFError { format!("Couldn't write to directory: {err}").into() })?;
+
+    let config = STATIC_DIR
+        .get_file(CONFIG_NAME)
+        .expect("static directory setup correctly")
+        .contents_utf8()
+        .expect("UTF-8 compliant");
+
+    let name = path
         .file_name()
-        .map(|i| i.to_str())
+        .expect("directory name")
+        .to_str()
         .ok_or("Directory name is not UTF-8 compliant")?;
 
-    STATIC_DIR
-        .extract(path)
-        .map_err(|err| -> BlogELFError { format!("Couldn't write to directory: {err}").into() })?;
+    let config = template::template_(
+        config,
+        HashMap::from([("name".to_string(), format!("\"{name}\""))]),
+    );
+
+    fs::write(path.join(CONFIG_NAME), config).map_err(|err| -> BlogELFError {
+        format!("Couldn't write to file '{CONFIG_NAME}': {err}").into()
+    })?;
 
     Ok(())
 }
